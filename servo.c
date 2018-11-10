1ae8f80202bc7bd6c77a2021723153f4ade56c0b
Rjfur <43212947+Rjfur@users.noreply.github.com>
2018-11-09 20:20:54 -0500

    Add files via upload

---
+++ servo.c
@@ -0,0 +1,46 @@
+/*
+* servo.c
+*
+* Created: 10/30/2018 3:34:40 PM
+*  Author: rjfur
+*/
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "servo.h"
+
+void T1pwm_Init(void)
+{
+
+	DDRB |= (1<<1);//set pin b1 as output my PWM pin
+
+	//Set port B as output/configure as 8 channels of outputs
+	// PB1 is now an output - corresponds to OCR1A
+
+	TCCR1A |= (1<<COM1A1) | (0<<COM1A0);
+	// set non-inverting mode
+
+	TCCR1A |= (1<<WGM11) | (0<<WGM10);
+	TCCR1B |= (1<<WGM12) | (1<<WGM13) | (1<<CS11);
+	// set fast PWM Mode 14
+
+	ICR1 = 39999; // For 20ms period
+
+
+	//TCCR0B |= (1<<CS11);
+	// set prescaler to 8 and starts PWM
+
+
+	//return 0
+}
+/*	while(1)
+{
+OCR1A = 1119; //+ offset; //2000 for 1ms pulse width
+// gives servo motor position
+_delay_ms(3000);
+OCR1A = 4899; //- offset;
+_delay_ms(3000);
+}*/
+
---
+++ servo.h
@@ -0,0 +1,17 @@
+/*
+ * servo.h
+ *
+ * Created: 10/30/2018 9:40:20 PM
+ *  Author: rjfur
+ */ 
+
+#include <avr/io.h>
+#ifndef SERVO_H_
+#define SERVO_H_
+
+
+void T1pwm_Init(void);
+
+
+
+#endif /* SERVO_H_ */
